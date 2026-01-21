ae3965a6b013a0f6ca6476b3a085ae53ed496b06
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-01-21 19:38:54 +0530

    added basic static-pages prj folder made with react

---
+++ static-pages/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+      <header>
+        <nav>
+          <img src="./src/assets/logo.png" alt="logo" />
+          <span>ReactFacts</span>
+        </nav>
+      </header>
+  )
+}
---
+++ static-pages/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+//renders 'App' component containing other components(navbar & content) inside it
+ReactDOM.createRoot(document.getElementById('root')).render( 
+    <App />
+)
+
